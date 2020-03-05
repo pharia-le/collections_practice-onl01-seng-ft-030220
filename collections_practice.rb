9c24cd72ab64499efd2be3c9dd4f7f749fb0c373
Pharia Le <pharia.le@gmail.com>
2020-03-05 08:13:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b }
+end
 
+def sort_array_desc(array)
+  array.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    
+end
+
+# def swap_elements_from_to(array, index, destination_index)
+  
+  
+# end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |ele| ele[0..1] + "$" + ele[3..-1] }
+end
+
+def find_a(array)
+  array.select { |string| string.start_with?("a") }
+end
+
+def sum_array(array)
+  
+end
